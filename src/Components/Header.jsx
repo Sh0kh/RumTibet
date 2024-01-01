dc3ad2d381509d30304a0f7d6ab50c77ac2011ef
Sh0kh <shohruuh07tuxtanazarov@gmail.com>
2024-01-02 00:52:36 +0500

    dqd

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
+// import React from 'react'
 import '../Style/Header.css'
 import header__logo from '../img/haeder__logo.png'
 import Hamburger from 'hamburger-react'
 function Header() {
 
- 
+    const [isIdAdded, setIdAdded] = useState(false);
+    const handleClick = () => {
+      setIdAdded(!isIdAdded);
+      console.log('ok');
+    };
+    const idName = isIdAdded ? 'my-element-id' : '';
+    const idName2 = isIdAdded ? 'my-element-id2' : '';
 
   return (
     <header>
@@ -35,16 +42,33 @@ function Header() {
                          Buy now
                     </button>
                 </div>
-                <div className='hamburger'>
-                <Hamburger onToggle={toggled => {
+                <div id={idName2} onClick={handleClick} className='hamburger'>
+                <Hamburger  onToggle={toggled => {
                     if (toggled) {
-                        // open a menu
+                        
                         
                     } else {
                         // close a menu
                     }
                     }} />
                 </div>
+            </div>
+            <div className='modal__medu' id={idName}>
+            <ul className='madal__list'>
+                        <li><a href="!#">About</a></li>
+                        <li><a href="!#">Features</a></li>
+                        <li><a href="!#">Pricing</a></li>
+                        <li><a href="!#">Team</a></li>
+                        <li><a href="!#">FAQ</a></li>
+                        <li><a href="!#">Feedback</a></li>
+                        <li><a href="!#">Blog</a></li>
+                        <li><a href="!#">Contact</a></li>
+                        <li >
+                            <button className='madal__btn'>
+                            Buy now
+                            </button>
+                        </li>
+                    </ul>
             </div>
         </div>
     </header>
